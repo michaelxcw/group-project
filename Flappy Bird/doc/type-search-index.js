dc583ddf31a70d3886eb974fdee9c090ff305e61
JONATHAN SU <jonasu9661328@gmail.com>
2022-01-13 04:39:27 +0000

    Delete Flappy Bird/doc directory

--- Flappy Bird/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bird"},{"p":"<Unnamed>","l":"EndScreen"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Leaderboard"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"StartScreen"},{"p":"<Unnamed>","l":"Tube"}];updateSearchResults();

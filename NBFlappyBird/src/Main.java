46589670b0fbfb9c45428cf2ccf63cffb71fb970
JONATHAN SU <jonasu9661328@gmail.com>
2022-01-17 15:32:04 -0700

    Add files via upload

---
+++ NBFlappyBird/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static StartScreen s = new StartScreen();
+    public static Game g = new Game();
+    public static EndScreen e = new EndScreen();
+    
+   public static void main(String[] args) {
+
+        s.setVisible(true);
+        g.setVisible(false);
+        e.setVisible(false);
+    }
+}
